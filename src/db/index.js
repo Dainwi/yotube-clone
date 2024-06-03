18e2bc875f9d2a64eff73c96a9db35a9af16c16e
Dainwi Kumar <danwichoudhary@gmail.com>
2024-06-03 15:25:19 +0530

    database connection problem solve

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
-    );
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(
       `\n Database connected || DB HOST: ${connectionInstance.connection.host}`
     );
--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-// require("dotenv").config({ path: "./env" });
-
-import dotenv from "dotenv";
-
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
 
 
-dotenv.config({
-    path: './env'
-})
-
 connectDB();
-
 
 /*
 import express from "express";
