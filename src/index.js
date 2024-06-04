e3a07c32aa318f66ecc1cab36cb20cf5c9b502a4
Dainwi Kumar <danwichoudhary@gmail.com>
2024-06-04 13:41:28 +0530

    utils code add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`app is listening on ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Database connection failed.... || ", err);
+  });
 
 /*
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+import { Promise } from "mongoose";
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+
+
+// we can also use this type 
+/*
+const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+*/
